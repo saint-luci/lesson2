0360fb81c3ce40f2a59f16f51a6124922e053b25
saint-luci <ese.worldworkshop@mail.ru>
2024-07-22 16:01:20 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+course_name = "Python"
+exercise_time = homework_count/hours_count
+print(course_name, ", всего задач:", homework_count, ", затрачено часов:", hours_count, ", среднее время выполнения:", exercise_time, "часа.")
